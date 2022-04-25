b2733b31bf63800530ac71c4efac21613e70eec1
PierreNS <pierreskinnerup@gmail.com>
2022-04-25 22:15:55 +0200

    [Player sprite] Added player sprite with animation.

--- DapperDasher/dasher.cpp
+++ DapperDasher/dasher.cpp
@@ -2,46 +2,79 @@
 
 int main()
 {
-    int windowWidth = 512;
-    int windowHeight = 380;
+#pragma region Window Fields
+    const int windowWidth = 512;
+    const int windowHeight = 380;
     const char* windowName = "Dapper Dash";
-
-    Vector2 rectSize {50,80};
-    Vector2 rectPos {((float)windowWidth/2)-(rectSize.x/2),(float)windowHeight-rectSize.y};
-    Rectangle rect{rectPos.x,rectPos.y,rectSize.x,rectSize.y};
     
-    int velocity = 0;
-    int gravity = 1;
-    int jumpForce = -22;
-
     InitWindow(windowWidth,windowHeight,windowName);
-
     SetTargetFPS(60);
+#pragma endregion
+/*================================================================================================================*/
+#pragma region Player Fields
+    Texture2D playerSprite = LoadTexture("./textures/scarfy.png");
+    Vector2 playerSpriteSize {128,128};
+    Vector2 playerPos {((float)windowWidth/2) - (playerSpriteSize.x/2),((float)windowHeight - playerSpriteSize.y)};
+    Rectangle playerRect{0,0,(float)playerSprite.width/6,(float)playerSprite.height};
+    
+    int animationFrame = 0;
+    const float animationUpdate = 1.0/12.0;
+    float animationTime = 0;
 
+    int velocity = 0;
+    const int gravity = 1000;
+    const int jumpForce = -600;
+
+    bool grounded;
+#pragma endregion
+/*================================================================================================================*/
+#pragma region Hazard Fields
+    
+#pragma endregion
+/*================================================================================================================*/
+    //Update loop...
     while (WindowShouldClose() == false)
     {        
+        float deltaTime = GetFrameTime();
+
         BeginDrawing();
         ClearBackground(WHITE);
 
-        //Game logic here...
-        if (rect.y >= windowHeight - rect.height)
+        if (playerPos.y >= windowHeight - playerRect.height)
         {
             velocity = 0;
-            rect.y = windowHeight - rect.height;
+            playerPos.y = windowHeight - playerRect.height;
+            grounded = true;
         }
         else
         {
-            velocity += gravity;
+            velocity += gravity * deltaTime;
+            grounded = false;
         }
         
-        if (IsKeyPressed(KEY_SPACE))
+        if (IsKeyPressed(KEY_SPACE) && grounded == true)
         {
             velocity = jumpForce;
         }
         
-        rect.y += velocity;
+        playerPos.y += velocity * deltaTime;
 
-        DrawRectangleRec(rect,BLUE);
+        DrawTextureRec(playerSprite,playerRect,playerPos,WHITE);
+
+        animationTime += deltaTime;
+
+        if (animationTime >= animationUpdate)
+        {
+            animationTime = 0;
+
+            playerRect.x = animationFrame * playerRect.width;
+            animationFrame++;
+
+            if (animationFrame > 5)
+            {
+                animationFrame = 0;
+            }
+        }
 
         EndDrawing();
     }
